9e5b352fd62e22d85848df2a1639a6091e1988db
Trent Reed <treed0803@gmail.com>
2015-02-05 20:41:42 -0800

    Qt5 OpenGL Part 14: Averaged Normals

---
+++ 14_AveragedNormals/includes/Karma/kgesture.h
@@ -0,0 +1,27 @@
+#ifndef KGESTURE_H
+#define KGESTURE_H
+
+#include <KPointF>
+#include <QGesture>
+
+class KGesture
+{
+public:
+  KGesture();
+  KGesture(QGesture const &rhs);
+  inline Qt::GestureState state() const;
+  inline KPointF hotSpot() const;
+  inline bool hasHotSpot() const;
+private:
+  bool m_hasHotSpot;
+  KPointF m_hotSpot;
+  Qt::GestureState m_gestureState;
+};
+
+inline KGesture::KGesture() : m_gestureState(Qt::NoGesture) {}
+inline KGesture::KGesture(QGesture const &rhs) : m_hasHotSpot(rhs.hasHotSpot()), m_hotSpot(rhs.hotSpot()), m_gestureState(rhs.state()) {}
+inline Qt::GestureState KGesture::state() const { return m_gestureState; }
+inline KPointF KGesture::hotSpot() const { return m_hotSpot; }
+inline bool KGesture::hasHotSpot() const { return m_hasHotSpot; }
+
+#endif // KGESTURE_H
